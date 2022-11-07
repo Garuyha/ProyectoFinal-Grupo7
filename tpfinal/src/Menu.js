fd16109ee0ceb5218846efd93128a8babb1e027d
Valeria Emilia Caballero <112204865+ValeriaEmilia000@users.noreply.github.com>
2022-11-07 11:46:08 -0300

    Menú (base)

---
+++ tpfinal/src/Menu.js
@@ -0,0 +1,25 @@
+
+
+  import React from "react";
+
+function Menu() {
+  return ( <>
+    <body>
+     <div id='cssmenu'>
+     <ul>
+     <li><a href='index.php'><span>Snake</span></a></li>
+     <li><a href='#'><span>Sopa de Letras</span></a></li>
+     <li><a href='#'><span>Arkanoid</span></a></li>
+     <li><a href='#'><span>Hangman</span></a></li>     
+     <li><a href="#"><span>Piedra, Papel o Tijera</span></a></li>
+     <li><a href="#"><span>Desarrolladores</span></a></li>   
+         
+     </ul>
+     </div>
+      
+    </body>
+  
+    </>
+  );
+}
+export default Menu;
