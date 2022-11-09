9147fb232a443c9ab5fdb6c6cb7d6ddbf7073f9b
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-11-09 18:17:08 -0300

    comit

---
+++ tpfinal/src/MemoriaApp.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App(){
+    return(
+        <div className = "App">
+            <h1>Juego de memoria</h1>
+            <buttom>Jugar</buttom>
+        </div>
+    );
+}
--- tpfinal/src/index.js
+++ tpfinal/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './JuegoE.js';
+import App from './MemoriaApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
