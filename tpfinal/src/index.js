73a0e377b816147d3aeb1f96f20fed38003ab946
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-11-09 22:04:18 -0300

    Index
    Correccion del index

--- tpfinal/src/index.js
+++ tpfinal/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
-//import App from './App';
-import MemoriaApp from './componentes/juegos/memoriaEzequiel/MemoriaApp.js';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <MemoriaApp />
+        <App />
       </BrowserRouter>
     </React.StrictMode>
 );
