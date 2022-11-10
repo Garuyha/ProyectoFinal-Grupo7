018aa0984bb3bbeef6fd39959b6d0cdf0793b64c
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-11-10 16:20:23 -0300

    Modificaciones
    Modificaciones visuales al juego de memoria e inicio de la implementación del sonido en el mismo.

--- tpfinal/src/index.js
+++ tpfinal/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MemoriaApp from './componentes/juegos/memoriaEzequiel/MemoriaApp'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <MemoriaApp />
       </BrowserRouter>
     </React.StrictMode>
 );
