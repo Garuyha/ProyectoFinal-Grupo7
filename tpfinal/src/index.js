c2f92cfab632d60539292f98166ea45af98674a8
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-11-10 21:24:25 -0300

    index

--- tpfinal/src/index.js
+++ tpfinal/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <MemoriaApp />
+        <App />
       </BrowserRouter>
     </React.StrictMode>
 );
