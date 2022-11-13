3fc65b961d754bd9d0c5ee181996e5bd0e38d4b7
HeNrYx94 <henryunju.fi@gmail.com>
2022-11-13 02:47:33 -0300

    dude

--- tpfinal/src/index.js
+++ tpfinal/src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import MemoriaApp from './componentes/juegos/memoriaEzequiel/MemoriaApp'
 import AntApp from './componentes/juegos/snakeIvan/AntApp'
 import AntHenryApp from './componentes/juegos/componenteJuego/AntHenryApp'
-import AppDude from './componentes/juegos/memoriaEzequiel/MemoriaApp';
+import DudeApp from './componentes/juegos/componenteJuego/dude/DudeApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <AppDude/>
+        <DudeApp/>
       </BrowserRouter>
     </React.StrictMode>
 );
