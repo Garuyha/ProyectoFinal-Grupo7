f2c2e0d9c87d5724230d4cba196f787812bf30e2
Carolina Maria de los Angeles Cortez <112413900+ChiaroCoure@users.noreply.github.com>
2022-11-13 05:06:07 -0300

    Dude
    Dude Dude Dude Dude Dude Dude Dude Dude

--- tpfinal/src/App.js
+++ tpfinal/src/App.js
@@ -2,10 +2,11 @@ import Header from "./componentes/Header";
 import { Routes, Route } from "react-router-dom";
 import Inicio from "./componentes/Inicio";
 import MenuPhaser from "./componentes/MenuPhaser";
-import HungryAnt from './componentes/juegos/snakeIvan/HungryAntGame'
 import MenuReact from "./componentes/MenuReact";
 import Desarrollador from "./componentes/Desarrollador";
 import Arkanoid from "./componentes/juegos/Arkanoid/Arkanoid";
+import HungryAnt from './componentes/juegos/snakeIvan/HungryAntGame'
+import JuegoDude from "./componentes/juegos/dude/DudeJuego";
 import Juego from "./componentes/juegos/PiedraPapelTijera/Juego";
 import Hangman from "./componentes/juegos/hangmanFuncion/Hangman";
 import MemoriaApp from "./componentes/juegos/memoriaEzequiel/MemoriaApp";
@@ -22,6 +23,7 @@ function App() {
         <Route path="/desarrollador" element={<Desarrollador />} />
         <Route path="/juegoarkaoid" element={<Arkanoid />} />
         <Route path="/juegohungry" element={<HungryAnt/>}/> 
+        <Route path="/juegohdude" element={<JuegoDude/>}/>
         <Route path="/juegoppt" element={<Juego />} />
         <Route path="/juegoahorcadito" element={<Hangman />} />
         <Route path="/memoriaapp" element={<MemoriaApp />} />
