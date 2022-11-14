8a451d411b51aae1d6bb40ed94dc665811b8657b
Carolina Maria de los Angeles Cortez <112413900+ChiaroCoure@users.noreply.github.com>
2022-11-13 21:16:19 -0300

    Errores solucionados

--- src/componentes/juegos/Arkanoid/AppArkanoid.js
+++ src/componentes/juegos/Arkanoid/AppArkanoid.js
@@ -1,11 +1,11 @@
 
 import Phaser from 'phaser';
-import Game from './componentes/Game.js';
+import NIvelNormal from './componentes/NIvelNormal';
 import { useEffect, useState } from 'react';
 import  { JuegoAcabado } from './componentes/GameOver.js'
 import { Ganaste } from './componentes/Victoria.js';
 import { Menu } from './componentes/Menu.js';
-import NivelDos  from './componentes/NivelDos.js';
+import NivelFacil  from './componentes/NivelFacil';
 
 function AppArkanoid() {
 
@@ -17,7 +17,7 @@ const config= {
 type: Phaser.AUTO,
 width:1300,                                            // Ancho de la pantalla
 height:750,                                           // Altura de la pantalla
-scene:[Menu,JuegoAcabado,Ganaste,Game,NivelDos],     // Se guardan las escenas del juego.
+scene:[Menu,JuegoAcabado,Ganaste,NIvelNormal,NivelFacil],     // Se guardan las escenas del juego.
 physics: {                                          // se habilita las fisicas del juego.
 default: 'arcade',
 arcade: {                                          
